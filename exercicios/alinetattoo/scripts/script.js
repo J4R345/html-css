13e9dc264db58639d287de4494d536ddff768c81
Jarbas Pereira de Souza <50222673+jarbassouza@users.noreply.github.com>
2022-02-23 22:10:06 -0300

    Aline Tattoo
    23022022

---
+++ exercicios/alinetattoo/scripts/script.js
@@ -0,0 +1,17 @@
+
+const theme = window.localStorage.getItem("theme");
+
+
+/* verifica se o tema armazenado no localStorage é escuro
+se sim aplica o tema escuro ao body */
+
+if (theme === "dark-mode") document.body.classList.add("dark-mode");
+
+
+ document.getElementById("btnDark").addEventListener("click", () =>{
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    if (theme === "dark-mode") {
+        window.localStorage.setItem("theme", "light-mode");
+      } else window.localStorage.setItem("theme", "dark-mode");
+ })
