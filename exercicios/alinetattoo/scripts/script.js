d104e1544c224f1c1524497777287cc2ac60c31b
Jarbas Pereira de Souza <50222673+jarbassouza@users.noreply.github.com>
2022-02-28 21:35:06 -0300

    Atualização
    28-02-2022

--- exercicios/alinetattoo/scripts/script.js
+++ exercicios/alinetattoo/scripts/script.js
@@ -1,5 +1,7 @@
 const theme = window.localStorage.getItem("theme");
 
+const btnModal = document.querySelector(".button-modal")
+const popUp = document.querySelector (".modal-promo")
 /* verifica se o tema armazenado no localStorage é escuro
 se sim aplica o tema escuro ao body */
 
@@ -12,4 +14,9 @@ if (theme === "dark-mode") document.body.classList.add("dark-mode");
     if (theme === "dark-mode") {
         window.localStorage.setItem("theme", "light-mode");
       } else window.localStorage.setItem("theme", "dark-mode");
+ })
+
+ btnModal.addEventListener ('click', () => {
+   popUp.style.display = 'none'
+
  })
